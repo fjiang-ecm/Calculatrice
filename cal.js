21427e71f0d273fd7f68ddf3d4ac08028aea4db2
FJ <francois.jiang@centrale-marseille.fr>
2020-01-01 15:21:22 +0100

    Suppressions

--- cal.js
+++ cal.js
@@ -3,3 +3,11 @@ var aff = document.getElementById("aff");
 function touche(str) {
     aff.textContent = aff.textContent + str;
 }
+function sup() {
+    aff.textContent = "";
+}
+function del() {
+    if (aff.textContent !== "") {
+        aff.textContent = aff.textContent.substr(0, aff.textContent.length - 1);
+    }
+}
--- cal.ts
+++ cal.ts
@@ -3,3 +3,13 @@ var aff: HTMLInputElement = <HTMLInputElement>document.getElementById("aff");
 function touche(str:string): void {
     aff.textContent = aff.textContent + str;
 }
+
+function sup(): void {
+    aff.textContent = "";
+}
+
+function del(): void {
+    if (aff.textContent !== "") {
+        aff.textContent = aff.textContent.substr(0, aff.textContent.length - 1);
+    }
+}
